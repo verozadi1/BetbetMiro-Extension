53af4f47f3f62288e3a09ff628c881ac4b238bd5
sad25kag <sad25kag@gmail.com>
2026-05-13 20:46:40 +0700

    Update YunshanidPlugin.kt

--- Yunshanid/src/main/kotlin/Yunshanid/YunshanidPlugin.kt
+++ Yunshanid/src/main/kotlin/Yunshanid/YunshanidPlugin.kt
@@ -5,7 +5,6 @@ import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class YunshanidPlugin : Plugin() {
-    // Kita balikkan ke load() tanpa parameter karena library kamu maunya begitu
     override fun load() {
         registerMainAPI(YunshanidProvider())
     }
