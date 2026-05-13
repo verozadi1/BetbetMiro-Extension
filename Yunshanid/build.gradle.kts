0b488cdf8c782660e59841ca70e9c47c6256a7c6
sad25kag <sad25kag@gmail.com>
2026-05-13 13:34:48 +0700

    Update build.gradle.kts

--- Yunshanid/build.gradle.kts
+++ Yunshanid/build.gradle.kts
@@ -1,16 +1,17 @@
-version = 1 // Bisa kamu naikkan kalau ada update
+version = 1
 
 cloudstream {
     language = "id"
     description = "Yunshanid — Streaming Movie, TV Series dan Anime Sub Indo"
     authors = listOf("BetbetMiro")
+
     status = 1
+
     tvTypes = listOf(
         "Movie",
         "TvSeries",
-        "Anime",
+        "Anime"
     )
 
-    // Favicon otomatis dari website Yunshanid
     iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://yunshanid.site&size=%size%"
 }
