7206ef824b1e09dcb4db71764ce1baf475076151
sad25kag <sad25kag@gmail.com>
2026-05-13 17:36:01 +0700

    Create Yunshanid/build.gradle.kts

---
+++ Yunshanid/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 1
+
+cloudstream {
+    language = "id"
+    authors = listOf("Betbet")
+
+    description = "Yunshanid Donghua Extension"
+    status = 1
+
+    tvTypes = listOf(
+        "Anime"
+    )
+
+    iconUrl = "https://yunshanid.site/favicon.ico"
+}
