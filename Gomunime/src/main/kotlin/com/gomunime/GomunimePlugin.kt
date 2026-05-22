145bf4a33193335b33b633c69e27fc8140de6df3
sad25kag <sad25kag@gmail.com>
2026-05-22 13:04:02 +0700

    chore(gomunime): disable provider temporarily Temporarily disable Gomunime from active builds by renaming its Gradle build file.
    Keep all provider source files in the repository so it can be restored later.
    
    Gomunime is disabled because homepage works but playback extraction still needs fixing

--- Gomunime/src/main/kotlin/com/gomunime/GomunimePlugin.kt
+++ Gomunime/src/main/kotlin/com/gomunime/GomunimePlugin.kt
@@ -6,7 +6,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class GomunimePlugin : Plugin() {
-//    override fun load(context: Context) {
-//        registerMainAPI(Gomunime())
+    override fun load(context: Context) {
+        registerMainAPI(Gomunime())
     }
 }
